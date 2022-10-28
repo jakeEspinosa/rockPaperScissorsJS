02444a83c39d41bb9b6be24d8fd84198f6edbb43
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2022-10-27 21:12:26 -0700

    Add playRound function.
    Added functionality to play a round. Returns 'draw', 'loss', and 'win',
    from the perspective of the player.

--- js/script.js
+++ js/script.js
@@ -9,3 +9,25 @@ function getComputerSelection() {
   let computerSelection = choicesArray[Math.floor(Math.random() * 3)];
   return computerSelection;
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+      return 'draw';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+      return 'loss';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'win';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'win';
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'draw';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'loss';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'loss';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'win';
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return 'draw';
+    } 
+}
