3960af89640998f3697ba6d127c85493f7518f0f
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2022-10-31 20:44:16 -0700

    Edit HTML and JS to get MVP working.
    Edited the HTML file and JS to get the MVP working. playRound() stops
    running once either the computer or the player's score is equal to 5.

--- js/script.js
+++ js/script.js
@@ -1,80 +1,105 @@
+let playerScore = 0;
+let computerScore = 0;
+let computerSelection;
+
+const playerScoreText = document.getElementById('your-score');
+const computerScoreText = document.getElementById('computer-score');
+const statusText = document.getElementById('status');
+
 function getComputerSelection() {
   let choicesArray = ['rock', 'paper', 'scissors']
   let computerSelection = choicesArray[Math.floor(Math.random() * 3)];
   return computerSelection;
 }
 
+function updateScreen(result) {
+  if (result === 'draw') {
+    statusText.textContent = 'It\s a draw!';
+  } else if (result === 'loss') {
+    computerScore++;
+    computerScoreText.textContent = `Computer score: ${computerScore}`;
+    statusText.textContent = `Sorry! Computer chose ${computerSelection}!`;
+  } else {
+    playerScore++;
+    playerScoreText.textContent = `Player score: ${playerScore}`;
+    statusText.textContent = `Nice! Computer chose ${computerSelection}!`;
+  }
+}
+
+function calculateWinner(score1, score2) {
+  if (score1 === 5) {
+    statusText.textContent = 'Congrats! You won!';
+  } else if (score2 === 5) {
+    statusText.textContent = 'You lost! Better luck next time!'
+  }
+}
+
 // return value is from the perspective of the player
 function playRound(playerSelection) {
-    let computerSelection = getComputerSelection();
+    computerSelection = getComputerSelection();
+    let roundResult;
     if (playerSelection === 'rock' && computerSelection === 'rock') {
-      resultsContainer.textContent = 'draw';
+      let roundResult = 'draw';
+      updateScreen(roundResult);
+      calculateWinner(playerScore, computerScore);
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-      resultsContainer.textContent = 'loss';
+      let roundResult = 'loss';
+      updateScreen(roundResult);
+      calculateWinner(playerScore, computerScore);
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-      resultsContainer.textContent = 'win';
+      let roundResult = 'win';
+      updateScreen(roundResult);
+      calculateWinner(playerScore, computerScore);
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-      resultsContainer.textContent = 'win';
+      let roundResult = 'win';
+      updateScreen(roundResult);
+      calculateWinner(playerScore, computerScore);
     } else if (playerSelection === 'paper' && computerSelection === 'paper') {
-      resultsContainer.textContent = 'draw';
+      let roundResult = 'draw';
+      updateScreen(roundResult);
+      calculateWinner(playerScore, computerScore);
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-      resultsContainer.textContent = 'loss';
+      let roundResult = 'loss';
+      updateScreen(roundResult);
+      calculateWinner(playerScore, computerScore);
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-      resultsContainer.textContent = 'loss';
+      let roundResult = 'loss';
+      updateScreen(roundResult);
+      calculateWinner(playerScore, computerScore);
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-      resultsContainer.textContent = 'win';
+      let roundResult = 'win';
+      updateScreen(roundResult);
+      calculateWinner(playerScore, computerScore);
     } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-      resultsContainer.textContent = 'draw';
+      let roundResult = 'draw';
+      updateScreen(roundResult);
+      calculateWinner(playerScore, computerScore);
     } 
 }
-
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  if (roundResult === 'win') {
-    console.log(`You won! The computer chose ${computerSelection}`);
-    playerScore++;
-  } else if (roundResult === 'loss') {
-    console.log(`You lost! The computer chose ${computerSelection}`);
-    computerScore++
-  } else {
-    console.log('It\'s a draw!');
-  }
-
-
-  if (playerScore > computerScore) {
-    console.log(`Your score: ${playerScore} \n` +
-                `Computer score: ${computerScore} \n` +
-                'Congrats! You won!');
-  } else if (playerScore < computerScore) {
-    console.log(`Your score: ${playerScore} \n` +
-                `Computer score: ${computerScore} \n` +
-                'Better luck next time! You lost!');
-  } else {
-    console.log(`Your score: ${playerScore} \n` +
-                `Computer score: ${computerScore} \n` +
-                'It\'s a draw!');
-  }
-}
-
 
 const rockButton = document.getElementById('rock');
 
 rockButton.addEventListener('click', () => {
+  if (playerScore === 5 || computerScore === 5) {
+    return;
+  }
   playRound('rock');
 });
 
 const paperButton = document.getElementById('paper');
 
 paperButton.addEventListener('click', () => {
+  if (playerScore === 5 || computerScore === 5) {
+    return;
+  }
   playRound('paper');
 });
 
 const scissorsButton = document.getElementById('scissors');
 
 scissorsButton.addEventListener('click', () => {
+  if (playerScore === 5 || computerScore === 5) {
+    return;
+  }
   playRound('scissors');
 });
-
-const resultsContainer = document.querySelector('#results-container');
